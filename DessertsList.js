a9b355a45da14246d5c782ed23baf18f2bc18dbc
mackinnonj <102379816+mackinnonj@users.noreply.github.com>
2023-02-09 07:29:39 -0800

    Create DessertsList.js

---
+++ DessertsList.js
@@ -0,0 +1,20 @@
+const DessertsList = (props) => {
+  // Implement the component here.
+  const dessertData = props.data
+    .filter((dessert) => {
+      return dessert.calories < 500;
+    })
+    .sort((a,b) => {
+      return a.calories - b.calories;
+    })
+    .map((dessert) => {
+      return (
+        <li>
+          {dessert.name} - {dessert.calories} cal
+        </li>
+      )
+    });
+  return <ul>{dessertData}</ul>
+}
+
+export default DessertsList;
